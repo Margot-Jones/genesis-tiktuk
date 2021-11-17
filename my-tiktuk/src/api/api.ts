087d75151d6c1cdb94b3fc21ab3fca1a58b750db
Hrinchenko Marharita <margot.hrinchenko@gmail.com>
2021-11-17 22:30:15 +0200

    create api calls

--- my-tiktuk/src/api/api.ts
+++ my-tiktuk/src/api/api.ts
@@ -1,4 +1,4 @@
-// import axios, { Method } from "axios";
+import axios from "axios";
 import { default as getFeed } from "../lib/getTrendingFeed.json";
 import { default as getUser } from "../lib/getUserInfo.json";
 import { default as getUserVideo } from "../lib/getUserFeed.json";
@@ -6,69 +6,47 @@ import { ItemList } from "../types/video";
 import { UserProp } from "../types/user";
 import { PostProp } from "../types/feed";
 
-export const getTrendingFeed = async (): Promise<PostProp[]> => {
-  //   const options = {
-  //     method: "GET" as Method,
-  //     url: "https://tiktok33.p.rapidapi.com/trending/feed",
-  //     headers: {
-  //       "x-rapidapi-host": "tiktok33.p.rapidapi.com",
-  //       "x-rapidapi-key": '151dd40a87msh1bd8ee1406e994cp1528c0jsna796a6e94e45',
-  //     },
-  //   };
+export const instance = axios.create({
+  baseURL: 'https://tiktok33.p.rapidapi.com',
+  headers: {
+    'x-rapidapi-host': 'tiktok33.p.rapidapi.com',
+    'x-rapidapi-key': process.env.REACT_APP_API_KEY as string,
+  },
+});
 
-  // axios
-  //   .request(options)
-  //   .then(function (response) {
-  //     console.log(response.data);
-  //     console.log("response.data");
-  //     return response.data;
-  //   })
-  //   .catch(function (error) {
-  //     console.error(error);
-  //   });
-  return getFeed;
+export const getTrendingFeed = async (): Promise<PostProp[]> => {
+  console.log('get Trending feed');
+
+  try {
+    const response = await instance.get('/trending/feed');
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    console.error('error');
+    return getFeed;
+  }
 };
 
 export const getUserInfo = async (username: string): Promise<UserProp> => {
-  //   const options = {
-  //     method: "GET" as Method,
-  //     url: "https://tiktok33.p.rapidapi.com/trending/feed",
-  //     headers: {
-  //       "x-rapidapi-host": "tiktok33.p.rapidapi.com",
-  //       "x-rapidapi-key": '151dd40a87msh1bd8ee1406e994cp1528c0jsna796a6e94e45',
-  //     },
-  //   };
-
-  // axios
-  //   .request(options)
-  //   .then(function (response) {
-  //     console.log(response.data);
-  //     console.log("response.data");
-  //     return response.data;
-  //   })
-  //   .catch(function (error) {
-  //     console.error(error);
-  //   });
-  return getUser;
+  try {
+    const response = await instance.get(`/user/info/${username}`);
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return getUser;
+  }
 };
 
-export const getUserFeedVideo = async (): Promise<ItemList[]> => {
-  // var axios = require("axios").default;
-
-  // var options = {
-  //   method: 'GET',
-  //   url: 'https://tiktok33.p.rapidapi.com/user/feed/dave.xp',
-  //   headers: {
-  //     'x-rapidapi-host': 'tiktok33.p.rapidapi.com',
-  //     'x-rapidapi-key': '151dd40a87msh1bd8ee1406e994cp1528c0jsna796a6e94e45'
-  //   }
-  // };
-
-  // axios.request(options).then(function (response) {
-  //   console.log(response.data);
-  // }).catch(function (error) {
+export const getUserFeedVideo = async (username='dave.xp'): Promise<ItemList[]> => {
+  // try {
+  //   const response = await instance.get(`/user/feed/${username}`);
+  //   console.log(response.data.itemList);
+  //   return response.data.itemList;
+  // } catch (error) {
   //   console.error(error);
-  // });
-
+  //   return getUserVideo.itemList;
+  // }
   return getUserVideo.itemList;
 };
