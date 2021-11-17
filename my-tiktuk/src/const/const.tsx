b603c35ed90d33763309783fde00ecbece7d693a
Hrinchenko Marharita <margot.hrinchenko@gmail.com>
2021-11-17 21:02:59 +0200

    create pagination

---
+++ my-tiktuk/src/const/const.tsx
@@ -0,0 +1,2 @@
+export const postsAmount = 30;
+export const postsPage = 3;
--- my-tiktuk/src/pages/Feed.tsx
+++ my-tiktuk/src/pages/Feed.tsx
@@ -1,39 +1,42 @@
 import { getTrendingFeed } from "../api/api";
 import Post from "../components/Post/Post";
-import Typography from '@mui/material/Typography';
-import Pagination from '@mui/material/Pagination';
-import Stack from '@mui/material/Stack';
-import React from "react";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
 import { useEffect, useState } from "react";
 import { PostProp } from "../types/feed";
 import { Grid } from "@mui/material";
-
+import { postsAmount, postsPage } from "../const/const";
 
 const Feed = () => {
-
   const [page, setPage] = useState(1);
   const [posts, setPosts] = useState<PostProp[]>([]);
 
   useEffect(() => {
-    const fetchMyAPI =  async () => {
+    const fetchMyAPI = async () => {
       // const response = await getTrendingFeed();
-      
+
       setPosts(await getTrendingFeed());
       console.log("await response");
-    }
+    };
 
-    fetchMyAPI()
-  }, [])
+    fetchMyAPI();
+  }, []);
 
   return (
-    <Grid display='flex' flexDirection='column' alignItems='center'>
+    <Grid display="flex" flexDirection="column" alignItems="center">
+      {posts
+        .slice(postsPage * (page - 1), postsPage * page)
+        .map((p, i) => (
+          <Post {...p} key={i} />
+        ))}
       <Stack spacing={2}>
-        <Typography>Page: {page}</Typography>
-        <Pagination count={10} page={page} onChange={(event, value) => setPage(value)} />
+        <Pagination
+          count={Math.ceil(postsAmount / postsPage)}
+          onChange={(e, page) => {
+            setPage(page);
+          }}
+        />
       </Stack>
-      {
-      posts.map((p, i) => <Post {...p} key={i}/>)
-      }
     </Grid>
   );
 };
